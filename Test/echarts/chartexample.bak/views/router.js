0dbf83aa6d5a65d4730c6c73825c897d91d336b2
zhoulihan <zhoulihan@zhumengyuan.com>
2016-02-29 19:46:57 +0800

    add file .

---
+++ Test/echarts/chartexample.bak/views/router.js
@@ -0,0 +1,32 @@
+/**
+ * Created by zhoulihan on 16-2-24.
+ */
+define(function(require){
+    require("$");
+    // 引入 ECharts 主模块
+    var echarts = require('../echarts/lib/echarts');
+// 引入柱状图
+    require('../echarts/lib/chart/bar');
+// 引入绘制直角坐标系需要的 Grid 组件
+    require('../echarts/lib/component/grid');
+// 引入提示框和标题组件
+    require('../echarts/lib/component/tooltip');
+    require('../echarts/lib/component/title');
+
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+    // 绘制图表
+    myChart.setOption({
+        title: { text: 'ECharts 入门示例' },
+        tooltip: {},
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    });
+})
